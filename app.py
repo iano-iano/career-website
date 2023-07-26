046fb8f4b5d719f1f18f86b0a0b629c586c06629
iano-iano <123099744+iano-iano@users.noreply.github.com>
2023-07-26 20:32:10 +0000

    Added json api

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+JOBS = [
+  {
+    "id": 1,
+    "title": "Data Scientist",
+    "salary": "ksh 500,000",
+    "location": "Nakuru",
+  },
+  {
+    "id": 2,
+    "title": "Frontend engineer",
+    "salary": "ksh 200,000",
+    "location": "Naivasha",
+  },
+  {
+    "id": 3,
+    "title": "Data Analyst",
+    "salary": "ksh 150,000",
+    "location": "Mombasa"
+  },
+  {
+    "id": 4,
+    "title": "Backend engineer",
+    "salary": "ksh 750,000",
+    "location": "Nairobi"
+  }
+]
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    return render_template("index.html",
+                          jobs=JOBS)
+
+@app.route("/api/jobs")
+def json():
+  return jsonify(JOBS)
 
 
 if __name__ == '__main__':
